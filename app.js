9f4ad742e7fbb26ffd95825cd8bc8ede85cc627b
meeniebeanie <minyen.j@gmail.com>
2016-09-12 13:06:43 +0800

    REACT RENDERED YAYYY

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('./config/express'),
     mongoose = require('./config/mongoose')
-    
+
 var db = mongoose();
 var app = express();
 
@@ -8,7 +8,7 @@ var app = express();
 app.set('port', (process.env.PORT || 3000));
 
 app.listen(app.get('port'), function() {
-  console.log('1\n2\n3\nS P L A S H', app.get('port'));
+  console.log('live on ', app.get('port'));
 });
 
 //export for jasmine test
