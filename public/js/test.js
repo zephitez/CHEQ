b03f1446681a18389127635f0d0fd7203440a3cd
Barnabas Chan <barny@post.com>
2016-09-13 18:10:29 +0800

    frontend-static

--- public/js/test.js
+++ public/js/test.js
@@ -5,16 +5,26 @@
           panelCount = carousel.children.length,
           theta = 0,
 
+
           onNavButtonClick = function( event ){
-            var increment = parseInt( event.target.getAttribute('data-increment') );
+            var increment;
+            if(event.target.id == "increment"){
+              increment = -1;
+            }else{
+            increment = 1;
+            }
+            // var increment = parseInt( event.target.getAttribute('data-increment') );
             theta += ( 360 / panelCount ) * increment * -1;
-            carousel.style.transform = 'translateZ( -288px ) rotateY(' + theta + 'deg)';
-            console.log(carousel.style.transform)
+            carousel.style.transform = 'translateZ( -344px ) rotateY(' + theta + 'deg)';
+            console.log("increment ", increment);
+            console.log("panelCount ", panelCount);
+            console.log("transform ", theta);
           };
-        console.log(carousel.style.transform)
+
       for (var i=0; i < 2; i++) {
         navButtons[i].addEventListener( 'click', onNavButtonClick, false);
       }
+      console.log("loaded");
 
     };
     window.addEventListener( 'DOMContentLoaded', init, false);
