b57cab3879c8f24187ad0141ac3ae3d39735fc87
Barnabas Chan <barny@post.com>
2016-10-05 22:34:59 +0800

    jumbotransition

--- public/js/test.js
+++ public/js/test.js
@@ -19,35 +19,35 @@ var init = function() {
 
 
 
-    // CAROUSEL
-    var carousel = document.getElementById('carousel'),
-        navButtons = document.querySelectorAll('#navigation button'),
-        panelCount = carousel.children.length,
-        theta = 0;
-
-
-    onArrowButtonClick = function( event ) {
-        var increment;
-
-        if(event.target.id == "increment") {
-          increment = -1;
-        } else {
-          increment = 1;
-        }
-        // var increment = parseInt( event.target.getAttribute('data-increment') );
-        theta += ( 360 / panelCount ) * increment * -1;
-
-        carousel.style.transform = 'translateZ( -344px ) rotateY(' + theta + 'deg)';
-
-        // testing purposes
-        console.log("increment ", increment);
-        console.log("panelCount ", panelCount);
-        console.log("transform ", theta);
-    };
-
-    for (var i=0; i < 2; i++) {
-      navButtons[i].addEventListener( 'click', onArrowButtonClick, false);
-    }
+    // // CAROUSEL
+    // var carousel = document.getElementById('carousel'),
+    //     navButtons = document.querySelectorAll('#navigation button'),
+    //     panelCount = carousel.children.length,
+    //     theta = 0;
+    //
+    //
+    // onArrowButtonClick = function( event ) {
+    //     var increment;
+    //
+    //     if(event.target.id == "increment") {
+    //       increment = -1;
+    //     } else {
+    //       increment = 1;
+    //     }
+    //     // var increment = parseInt( event.target.getAttribute('data-increment') );
+    //     theta += ( 360 / panelCount ) * increment * -1;
+    //
+    //     carousel.style.transform = 'translateZ( -344px ) rotateY(' + theta + 'deg)';
+    //
+    //     // testing purposes
+    //     console.log("increment ", increment);
+    //     console.log("panelCount ", panelCount);
+    //     console.log("transform ", theta);
+    // };
+    //
+    // for (var i=0; i < 2; i++) {
+    //   navButtons[i].addEventListener( 'click', onArrowButtonClick, false);
+    // }
 
 
 
